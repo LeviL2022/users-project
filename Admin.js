3099b195cfde2a8ec9b9034d7e70b1185a2e0707
Levi Lucas <levilucasaraujolopes@gmail.com>
2022-06-20 18:36:26 -0300

    Adicionando restante do Projeto

--- Admin.js
+++ Admin.js
@@ -1,11 +1,15 @@
 import User from "./User.js";
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo)
     }
-}
 
-const novoAdmin = new Admin('João', 'joa@j.com', '2035/12/13')
-console.log(novoAdmin)
-console.log(novoAdmin.exibirInfos())
+    exibirInfos() {
+        return `${this.nome}, ${this.role}`
+    }
+
+    criarCurso(nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
+    }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import User from "./User.js"
+import Admin from "./Admin.js"
+import Docente from "./Docente.js"
+
+//const novoUser = new User('Mariana', 'm@m.com', '2005/04/06')
+
+const novoAdmin = new Admin('Rodrigo', 'R@r', '2007/05/06/')
+console.log(novoAdmin.nome)
